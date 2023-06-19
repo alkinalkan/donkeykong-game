c78d9b29a4949b9f1373049c89d164ffc0d102eb
Alkın Alkan <83280716+alkinalkan@users.noreply.github.com>
2023-06-19 18:58:40 +0300

    Comment Update Display.java

--- src/DonkeyKong/Display.java
+++ src/DonkeyKong/Display.java
@@ -3,14 +3,13 @@ package DonkeyKong;
 import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-
 import javax.swing.JFrame;
 
 public class Display {
-	private JFrame frame;
-	private String title;
-	private int height, widht;
-	private Canvas canvas;
+	private JFrame frame; // The JFrame object for the game window
+	private String title; // The title of the game window
+	private int height, widht; // The dimensions of the game window
+	private Canvas canvas; // The canvas where the game will be rendered
 
 	public Display(String title, int height, int widht) {
 		this.title = title;
@@ -19,35 +18,37 @@ public class Display {
 		createDisplay();
 	}
 
+	// Create and initialize the display window
 	private void createDisplay() {
+		// Create the JFrame
 		frame = new JFrame(title);
 		frame.setSize(widht, height);
 
+		// Set the icon image of the frame
 		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("logo.png")));
 
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // çarpý ile oyun kapatma
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
-		frame.setLocationRelativeTo(null); // Oyunu ortada baþlatna
+		frame.setLocationRelativeTo(null); // Center the window on the screen
 		frame.setVisible(true);
 
-		// CANVAS ALEMLERÄ°
-
+		// Create the canvas
 		canvas = new Canvas();
 		canvas.setPreferredSize(new Dimension(widht, height));
 		canvas.setMaximumSize(new Dimension(widht, height));
 		canvas.setMinimumSize(new Dimension(widht, height));
 		frame.add(canvas);
-		frame.pack(); // Komut almama ihtimalini sýfýrlar
-		canvas.setFocusable(false);
+		frame.pack(); // Adjust the frame size to fit the canvas
+		canvas.setFocusable(false); // Prevent the canvas from automatically receiving focus
 	}
 
+	// Get the canvas object
 	public Canvas getCanvas() {
 		return canvas;
 	}
 
-	// Jframelerime ulaþmak için
+	// Get the JFrame associated with the display
 	public JFrame getFrame() {
 		return frame;
 	}
-
 }
