4c08684e8a82daa8e4e65f43f5934a77c714adec
Alkın Alkan <83280716+alkinalkan@users.noreply.github.com>
2023-07-06 23:51:00 +0300

    Comment Update Winstate.java

--- src/DonkeyKong/Winstate.java
+++ src/DonkeyKong/Winstate.java
@@ -5,25 +5,25 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 public class Winstate extends State {
-	private BufferedImage win;
+	private BufferedImage win; // Image to display when the player wins
 
 	public Winstate(Game game) {
 		super(game);
-		win = ImageLoader.loadImage("final.png");
+		win = ImageLoader.loadImage("final.png"); // Load the win image from a file
 	}
 
 	@Override
 	public void tick() {
-		if (game.getKeyManager().res) {
-			State.setState(game.menuState);
+		if (game.getKeyManager().res) { // If the "res" key is pressed
+			State.setState(game.menuState); // Go back to the menu state
 		}
 	}
 
 	@Override
 	public void render(Graphics g) {
 		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, 600, 800);
-		g.drawImage(win, 0, 0, 600, 640, null);
+		g.fillRect(0, 0, 600, 800); // Fill the background with black color
+		g.drawImage(win, 0, 0, 600, 640, null); // Draw the win image on the screen
 	}
 
 }
