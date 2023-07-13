4dacd115be5622b6760fa7ec38520115a7ce935b
Alkın Alkan <83280716+alkinalkan@users.noreply.github.com>
2023-07-13 23:11:57 +0300

    Comment Update MenuState.java

--- src/DonkeyKong/MenuState.java
+++ src/DonkeyKong/MenuState.java
@@ -19,10 +19,14 @@ public class MenuState extends State {
 	public MenuState(Game game) {
 		super(game);
 		random = new Random();
+		
+		// Load Kong sprites from the sprite sheet
 		kong = new BufferedImage[2];
 		kong[0] = sheet.crop(58, 152, 46, 32);
 		kong[1] = sheet.crop(202, 152, 46, 32);
 		kongAnim = new Animation(300, kong);
+		
+		// Load barrel sprites from the sprite sheet
 		barrelb = new BufferedImage[4];
 		barrelb[0] = sheet.crop(66, 258, 12, 10);
 		barrelb[1] = sheet.crop(81, 258, 12, 10);
@@ -33,8 +37,11 @@ public class MenuState extends State {
 
 	@Override
 	public void tick() {
+		// Update animations
 		kongAnim.tick();
 		barrela.tick();
+		
+		// Check if the start key is pressed to switch to the game state
 		if (game.getKeyManager().start) {
 			State.setState(game.gameState);
 		}
@@ -42,25 +49,38 @@ public class MenuState extends State {
 
 	@Override
 	public void render(Graphics g) {
+		// Clear the screen
 		g.clearRect(0, 0, 600, 800);
+		
+		// Set the background color
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, 600, 800);
+		
+		// Render Kong animation
 		g.drawImage(kongAnim.getCurrentFrame(), 235, 90, 100, 100, null);
+		
+		// Generate random RGB values for barrel color
 		float r = (float) (random.nextFloat());
 		float s = (float) (random.nextFloat());
 		float b = (float) (random.nextFloat());
+		
+		// Render barrel animations
 		g.drawImage(barrela.getCurrentFrame(), 30, 90, 100, 100, null);
 		g.drawImage(barrela.getCurrentFrame(), 470, 90, 100, 100, null);
+		
+		// Set font and color for text
 		g.setColor(new Color(r, s, b));
 		g.setFont(new Font("TimesRoman PLAIN", Font.BOLD, 40));
+		
+		// Render game title and credits
 		g.drawString("DONKEY KONG", 140, 70);
-		g.drawString("Alk˝n ALKAN", 165, 240);
+		g.drawString("Alk√Ωn ALKAN", 165, 240);
 		g.drawString("Doruk ARSLAN", 145, 310);
-		g.drawString("Melih BENL›O–LU", 120, 380);
-		g.drawString("Douhan CUMAO–LU", 100, 450);
+		g.drawString("Melih BENL√ùO√êLU", 120, 380);
+		g.drawString("Do√∞uhan CUMAO√êLU", 100, 450);
+		
+		// Render start prompt
 		g.setColor(Color.RED);
 		g.drawString("Press enter to play", 125, 600);
-
 	}
-
 }
