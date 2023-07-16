6ae66bce6e7c9c8aaf7f08bcad860078781c6f66
Alkın Alkan <83280716+alkinalkan@users.noreply.github.com>
2023-07-16 23:49:28 +0300

    Comment Update Ladder.java

--- src/DonkeyKong/Ladder.java
+++ src/DonkeyKong/Ladder.java
@@ -1,51 +1,53 @@
-package DonkeyKong;
+package DonkeyKong; // The class belongs to the package DonkeyKong
 
 public class Ladder {
-	public int x;
-	public int ybottom;
-	public int height;
-	public int width;
+    public int x; // X-coordinate of the ladder's top-left corner
+    public int ybottom; // Y-coordinate of the ladder's bottom-right corner
+    public int height; // The height of the ladder
+    public int width; // The width of the ladder
 
-	public Ladder(int x, int y, int width, int height) {
-		this.width = width;
-		this.height = height;
-		this.x = x;
-		this.ybottom = y;
-	}
-	public void creation() {
+    // Constructor to initialize the ladder with its position and dimensions
+    public Ladder(int x, int y, int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.ybottom = y;
+    }
 
-	}
+    // The creation() method seems to be empty and doesn't perform any action.
+    // It might be intended to handle the ladder creation process in the game.
 
-	public int getX() {
-		return x;
-	}
+    // Getter and setter methods for the ladder's properties
 
-	public void setX(int x) {
-		this.x = x;
-	}
+    public int getX() {
+        return x;
+    }
 
-	public int getYbottom() {
-		return ybottom;
-	}
+    public void setX(int x) {
+        this.x = x;
+    }
 
-	public void setYbottom(int ybottom) {
-		this.ybottom = ybottom;
-	}
+    public int getYbottom() {
+        return ybottom;
+    }
 
-	public int getHeight() {
-		return height;
-	}
+    public void setYbottom(int ybottom) {
+        this.ybottom = ybottom;
+    }
 
-	public void setHeight(int height) {
-		this.height = height;
-	}
+    public int getHeight() {
+        return height;
+    }
 
-	public int getWidth() {
-		return width;
-	}
+    public void setHeight(int height) {
+        this.height = height;
+    }
 
-	public void setWidth(int width) {
-		this.width = width;
-	}
+    public int getWidth() {
+        return width;
+    }
 
+    public void setWidth(int width) {
+        this.width = width;
+    }
 }
