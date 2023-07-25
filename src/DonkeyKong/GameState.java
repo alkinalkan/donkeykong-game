b561704ba46f37f6c40017af23a37707fc46ea84
Alkın Alkan <83280716+alkinalkan@users.noreply.github.com>
2023-07-25 23:45:34 +0300

    Comment Update GameState.java

--- src/DonkeyKong/GameState.java
+++ src/DonkeyKong/GameState.java
@@ -3,31 +3,39 @@ package DonkeyKong;
 import java.awt.Graphics;
 
 public class GameState extends State {
-	Game game;
-	private Mario mario;
-	private Kong kong;
-	private Barrel barrel;
+    Game game;
+    private Mario mario;
+    private Kong kong;
+    private Barrel barrel;
 
-	public GameState(Game game) {
-		super(game);
-		barrel = new Barrel();
-		mario = new Mario(game, barrel, 50, 750, 39, 28);
-		kong = new Kong(game, 0, 760);
-	}
+    public GameState(Game game) {
+        // Call the constructor of the parent class (State) passing the 'game' parameter
+        super(game);
+        
+        // Create a new Barrel object
+        barrel = new Barrel();
+        
+        // Create a new Mario object with initial position (50, 750) and size (39, 28)
+        // Pass the 'game' and 'barrel' objects as parameters to Mario's constructor
+        mario = new Mario(game, barrel, 50, 750, 39, 28);
+        
+        // Create a new Kong object with initial position (0, 760)
+        kong = new Kong(game, 0, 760);
+    }
 
-	@Override
-	public void tick() {
-		mario.tick();
-		barrel.tick();
-		kong.tick();
-	}
+    @Override
+    public void tick() {
+        // Call the tick method for each object to update their state
+        mario.tick();
+        barrel.tick();
+        kong.tick();
+    }
 
-	@Override
-	public void render(Graphics g) {
-		mario.render(g);
-		barrel.render(g);
-		kong.render(g);
-
-	}
-
+    @Override
+    public void render(Graphics g) {
+        // Call the render method for each object to draw them on the screen
+        mario.render(g);
+        barrel.render(g);
+        kong.render(g);
+    }
 }
