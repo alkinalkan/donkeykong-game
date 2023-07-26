e9b429800203237f5935984d1cc25b22c8c98de2
Alkın Alkan <83280716+alkinalkan@users.noreply.github.com>
2023-07-26 12:24:49 +0300

    Comment Update Launcher.java

--- src/DonkeyKong/Launcher.java
+++ src/DonkeyKong/Launcher.java
@@ -1,10 +1,21 @@
+// The "DonkeyKong" package contains the classes related to the Donkey Kong game.
 package DonkeyKong;
 
+// The Launcher class serves as the entry point for the Donkey Kong game.
 public class Launcher {
 
-	public static void main(String[] args) {
-		Game game = new Game("Donkey Kong", 800, 600);
-		game.start();
-	}
+    // The main method is the starting point of the application.
+    public static void main(String[] args) {
 
+        // Create a new instance of the Game class, which represents the Donkey Kong game.
+        // The constructor of the Game class takes three arguments:
+        // 1. The title of the game ("Donkey Kong" in this case).
+        // 2. The width of the game window (800 pixels).
+        // 3. The height of the game window (600 pixels).
+        Game game = new Game("Donkey Kong", 800, 600);
+
+        // Call the start() method of the Game class to begin the game.
+        // This method will initialize the game, create the game window, and start the game loop.
+        game.start();
+    }
 }
