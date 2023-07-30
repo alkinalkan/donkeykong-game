598194d0c320557bcc41bce1df53874d0aa3e109
Alkın Alkan <83280716+alkinalkan@users.noreply.github.com>
2023-07-30 16:36:10 +0300

    Comment Update GameOver.java

--- src/DonkeyKong/GameOver.java
+++ src/DonkeyKong/GameOver.java
@@ -10,25 +10,48 @@ import java.awt.image.BufferedImage;
  * When the restart key is pressed, it transitions the game state to the menu state.
  */
 public class GameOver extends State {
-	private BufferedImage lose;
 
-	public GameOver(Game game) {
-		super(game);
-		lose = ImageLoader.loadImage("lose.png");
-	}
+    // The image displayed when the player loses the game
+    private BufferedImage lose;
 
-	@Override
-	public void tick() {
-		if (game.getKeyManager().res) {
-			State.setState(game.menuState);
-		}
-	}
+    /**
+     * Constructor for the GameOver class.
+     *
+     * @param game The Game object that manages the game's state.
+     */
+    public GameOver(Game game) {
+        // Call the constructor of the parent class (State) to set up the game reference
+        super(game);
+        // Load the "lose.png" image to display on the game over screen
+        lose = ImageLoader.loadImage("lose.png");
+    }
 
-	@Override
-	public void render(Graphics g) {
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, 600, 800);
-		g.drawImage(lose, 0, 0, 600, 640, null);
-	}
+    /**
+     * Update method for the GameOver state.
+     * It checks if the restart key is pressed, and if so, transitions the game state to the menu state.
+     */
+    @Override
+    public void tick() {
+        // Check if the restart key is pressed (assumed to be stored in the game's key manager)
+        if (game.getKeyManager().res) {
+            // Set the game state to the menu state to restart the game
+            State.setState(game.menuState);
+        }
+    }
 
+    /**
+     * Render method for the GameOver state.
+     * It displays the game over screen with a black background and the "lose.png" image on top.
+     *
+     * @param g The Graphics object used for rendering.
+     */
+    @Override
+    public void render(Graphics g) {
+        // Set the background color to black
+        g.setColor(Color.BLACK);
+        // Fill the entire screen with the black color to clear any previous state's rendering
+        g.fillRect(0, 0, 600, 800);
+        // Draw the "lose.png" image on the game over screen, scaled to fit the screen size (600x640)
+        g.drawImage(lose, 0, 0, 600, 640, null);
+    }
 }
