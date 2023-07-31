180990e0e3aa8ff63f85005f0269d95776468bb8
Alkın Alkan <83280716+alkinalkan@users.noreply.github.com>
2023-07-31 18:15:19 +0300

    Comment Update Barrel.java

--- src/DonkeyKong/Barrel.java
+++ src/DonkeyKong/Barrel.java
@@ -5,10 +5,11 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class Barrel {
+	// Initial position and movement variables for the barrel
 	public int x = 120; // Initial x position
 	public int y = 50; // Initial y position
-	public int height;
-	public int widht; // Typo: should be 'width'
+	public int height; // Height of the barrel (not used in this code)
+	public int widht; // Typo: should be 'width' (not used in this code)
 	public int barrelMoveX = 2; // Barrel movement speed along the x-axis
 	private int r = 0; // Counter variable for barrel movement
 	public ArrayList<Barrel> Barrels = new ArrayList<Barrel>(); // List to hold multiple barrels
@@ -16,10 +17,11 @@ public class Barrel {
 
 	public static BufferedImage[] barrelb;
 	SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("a.png")); // Loading sprite sheet
-	private Animation barrela;
+	private Animation barrela; // Animation for the barrel
 
 	public Barrel() {
 		barrelb = new BufferedImage[4]; // Array to hold barrel images
+		// Crop the barrel images from the sprite sheet
 		barrelb[0] = sheet.crop(66, 258, 12, 10); // Crop the first barrel image from the sprite sheet
 		barrelb[1] = sheet.crop(81, 258, 12, 10); // Crop the second barrel image from the sprite sheet
 		barrelb[2] = sheet.crop(66, 270, 12, 10); // Crop the third barrel image from the sprite sheet
@@ -40,9 +42,9 @@ public class Barrel {
 
 			// Reset the position and change the direction after 35 steps
 			if (r == 35) {
-				this.setX(this.getX() - this.getBarrelMoveX());
+				this.setX(this.getX() - this.getBarrelMoveX()); // Move the barrel back to its original position
 				r = 0;
-				this.setBarrelMoveX((-1) * this.getBarrelMoveX());
+				this.setBarrelMoveX((-1) * this.getBarrelMoveX()); // Change the barrel movement direction
 			}
 		}
 	}
@@ -72,47 +74,3 @@ public class Barrel {
 	}
 
 	public int getWidth() {
-		return widht; // Typo: should be 'width'
-	}
-
-	public void setWidth(int width) {
-		this.widht = width; // Typo: should be 'width'
-	}
-
-	public int getBarrelMoveX() {
-		return barrelMoveX;
-	}
-
-	public void setBarrelMoveX(int barrelMoveX) {
-		this.barrelMoveX = barrelMoveX;
-	}
-
-	public void tick() {
-		barrela.tick(); // Update the animation
-
-		if (timer == 350) {
-			// Add a new barrel to the list every 350 ticks
-			Barrel newBarrel = new Barrel();
-			Barrels.add(newBarrel);
-			timer = 0;
-		}
-
-		if (Barrels.size() == 7) {
-			// Remove the oldest barrel from the list when there are already 7 barrels
-			Barrels.remove(0);
-		}
-		timer++;
-
-		// Move all the barrels in the list
-		for (int i = 0; i < Barrels.size(); i++) {
-			Barrels.get(i).move();
-		}
-	}
-
-	public void render(Graphics g) {
-		for (int i = 0; i < Barrels.size(); i++) {
-			// Draw the barrels on the screen
-			g.drawImage(barrela.getCurrentFrame(), Barrels.get(i).getX(), Barrels.get(i).getY(), 30, 30, null);
-		}
-	}
-}
